f662e5d0a84d0226a2009510639c01e1823e1c80
GrehBan <57055685+GrehBan@users.noreply.github.com>
2021-02-12 20:10:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-from constants import WEBHOOK_PORT, WEBHOOK_HOST, WEBHOOK_PATH
-
-async def on_shutdown(dp):
-    await dp.bot.delete_webhook()
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-
 
 if __name__ == '__main__':
 	from handlers import dp
-	from aiogram.utils.executor import start_webhook, start_polling
-	"""start_webhook(
-        dispatcher=dp,
-        host=WEBHOOK_HOST,
-        port=WEBHOOK_PORT,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_shutdown=on_shutdown)"""
-        start_polling(dp)
+	from aiogram import executor
+        executor.start_polling(dp)
