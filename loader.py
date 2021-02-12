55a78be8304130b13b0719850d995aa0bb7d469d
GrehBan <57055685+GrehBan@users.noreply.github.com>
2021-02-12 19:46:29 +0100

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,10 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from constants import TOKEN
 import logging
 
 bot = Bot(token=TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
 dp.middleware.setup(LoggingMiddleware())
