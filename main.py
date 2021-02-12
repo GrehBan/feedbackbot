79dacbeaad5c8c9f067f0b3d0496e1fb0de6c892
GrehBan <57055685+GrehBan@users.noreply.github.com>
2021-02-12 20:07:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ async def on_shutdown(dp):
 
 if __name__ == '__main__':
 	from handlers import dp
-	from aiogram.utils.executor import start_webhook
-	start_webhook(
+	from aiogram.utils.executor import start_webhook, start_polling
+	"""start_webhook(
         dispatcher=dp,
         host=WEBHOOK_HOST,
         port=WEBHOOK_PORT,
         webhook_path=WEBHOOK_PATH,
         skip_updates=True,
-        on_shutdown=on_shutdown)
+        on_shutdown=on_shutdown)"""
+        start_polling(dp)
